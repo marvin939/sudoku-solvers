4c14744a3418ab4caa59c47a52269c0996789398
marvin <marvinCEF@gmail.com>
2025-05-28 22:54:32 +1200

    Added command line parameters to norvigsudoku and ability to read files containing multiple sudoku puzzles in the format of p096_sudoku.txt or top95.txt

--- norvigsudoku.nim
+++ norvigsudoku.nim
@@ -9,6 +9,8 @@ import times
 import tables
 import sets
 import sugar
+import os
+import parseopt
 
 # let INITIAL_GRID = [
 #     [5, 3, 0, 0, 7, 0, 0, 0, 0],
@@ -48,8 +50,8 @@ let INITIAL_GRID = [
 #     [0, 0, 5, 0, 1, 0, 3, 0, 0],
 # ]
 
-type
-    Grid = array[9, array[9, int]]
+# type
+#     Grid = array[9, array[9, int]]
 
 # let INITIAL_GRID = [
 #     [0, 0, 3, 0, 2, 0, 6, 0, 0],
@@ -64,28 +66,28 @@ type
 # ]
 
 
-proc `$`(grid: Grid): string =
-    var lines: seq[string] = @[]
-    for y in 0 ..< grid.len:
-        if y mod 3 == 0 and y > 0:
-            lines.add("-------+-------+-------")
+# proc `$`(grid: Grid): string =
+#     var lines: seq[string] = @[]
+#     for y in 0 ..< grid.len:
+#         if y mod 3 == 0 and y > 0:
+#             lines.add("-------+-------+-------")
         
-        let row = grid[y]       
-        var rowStr: string
-        for x, c in row:
-            # echo x
-            let divisibleBy3 = x mod 3 == 0
-            if divisibleBy3 and x > 0:
-                rowStr.add(" |")
-            if c == 0:
-                rowStr.add("  ")  # blank
-            else:
-                rowStr.add(" " & $c)
-        lines.add(rowStr)
-    result = lines.join("\n")
+#         let row = grid[y]       
+#         var rowStr: string
+#         for x, c in row:
+#             # echo x
+#             let divisibleBy3 = x mod 3 == 0
+#             if divisibleBy3 and x > 0:
+#                 rowStr.add(" |")
+#             if c == 0:
+#                 rowStr.add("  ")  # blank
+#             else:
+#                 rowStr.add(" " & $c)
+#         lines.add(rowStr)
+#     result = lines.join("\n")
 
-proc displayGrid(grid: Grid) =
-    echo $grid
+# proc displayGrid(grid: Grid) =
+#     echo $grid
 
 
 #[
@@ -289,12 +291,15 @@ proc test() =
 
 test()
 
-# proc assign(values: Table[string, string], s: string, d: string): Table[string, string]
-proc assign(values: var Table[string, string], s: string, d: string): bool
-proc grid_values(grid: string): Table[string, string]
-proc eliminate(values: var Table[string, string], s: string, d: string): bool
+type
+    GridValues = Table[string, string]
 
-proc parse_grid(grid: string): Table[string, string] =
+# proc assign(values: Table[string, string], s: string, d: string): Table[string, string]
+proc assign(values: var GridValues, s: string, d: string): bool
+proc grid_values(grid: string): GridValues
+proc eliminate(values: var GridValues, s: string, d: string): bool
+
+proc parse_grid*(grid: string): GridValues =
     #[Convert grid to a dict of possible values, {square: digits}, or 
     return False if a contradiction is detected.]#
     var values: Table[string, string]
@@ -307,7 +312,7 @@ proc parse_grid(grid: string): Table[string, string] =
     return values
 
 
-proc grid_values(grid: string): Table[string, string] =
+proc grid_values(grid: string): GridValues =
     #[ Convert grid into a table of {square: char} with '0' or '.' for empties. ]#
     var chars: string
     for c in grid:
@@ -319,7 +324,7 @@ proc grid_values(grid: string): Table[string, string] =
     # echo "grid_values: ", result
     return result
 
-proc assign(values: var Table[string, string], s: string, d: string): bool =
+proc assign(values: var GridValues, s: string, d: string): bool =
     #[ Eliminitae all other values (except d) from values[s] and propagate.
     Return values, except return False if a contradiction is detected. ]#
     let other_values = values[s].replace(d, "")
@@ -340,70 +345,7 @@ proc assign(values: var Table[string, string], s: string, d: string): bool =
     return true
 
 
-# proc eliminate(values: Table[string, string], s: string, d: string): Table[string, string] =
-#     #[ Eliminate d from values[s]; propagate when values or places <= 2.
-#     Return values, except return False if a contradiction is detected. ]#
-#     if not ( d in values[s]):
-#         return result  # Already eliminated
-#     values[s] = values[s].replace(d, "")
-#     # (1) If a square s is reduced to one value, then eliminate d2 from the peers.
-#     if len(values[s]) == 0:
-#         return result  # Contradiction: removed last value
-#     elif len(values[s]) == 1:
-#         let d2 = values[s]
-#         if not peers[s].all(proc (s2: string): bool = eliminate(values, s2, d2).len > 0):
-#             return result
-#     # (2) If a unit u is reduced to only one place for a value d, then put it there.
-#     for u in units[s]:
-#         var dplaces: seq[string]
-#         for s in u:
-#             if d in values[s]: dplaces.add(s)
-#         if len(dplaces) == 0:
-#             return result  # Contradiction: no place for this value
-#         elif len(dplaces) == 1:
-#             # d can only be in one place in unit; assign it there
-#             if not assign(values, dplaces[0], d):
-#                 return result
-#     return values
-
-# Doesn't work ^^^ - errors when attempting to modify values parameter (immutable)
-
-# proc eliminate(values: Table[string, string], s: string, d: string): Table[string, string] =
-#     #[ Eliminate d from values[s]; propagate when values or places <= 2.
-#     Return values, except return empty table if a contradiction is detected. ]#
-#     var tempValues: Table[string, string] = values
-#     if not (d in tempValues[s]):
-#         return tempValues  # Already eliminated
-#     tempValues[s] = tempValues[s].replace(d, "")
-#     # (1) If a square s is reduced to one value, then eliminate d2 from the peers.
-#     if len(tempValues[s]) == 0:
-#         tempValues.clear()
-#         return tempValues  # Contradiction: removed last value
-#     elif len(tempValues[s]) == 1:
-#         let d2 = tempValues[s]
-#         #if not peers[s].all(proc (s2: string): bool = return (eliminate(tempValues, s2, d2).len > 0)):
-#         # if not peers[s].map(proc (s2: string): bool = eliminate(tempValues, s2, d2).len > 0).all():
-#         if not all(peers[s], proc (s2: string): bool = return (eliminate(tempValues, s2, d2).len > 0)):
-#             tempValues.clear()
-#             return tempValues
-#     # (2) If a unit u is reduced to only one place for a value d, then put it there.
-#     for u in units[s]:
-#         var dplaces: seq[string]
-#         for s in u:
-#             if d in tempValues[s]: dplaces.add(s)
-#         if len(dplaces) == 0:
-#             tempValues.clear()
-#             return tempValues  # Contradiction: no place for this value
-#         elif len(dplaces) == 1:
-#             # d can only be in one place in unit; assign it there
-#             # if not assign(tempValues, dplaces[0], d):
-#             if assign(tempValues, dplaces[0], d).len == 0:
-#                 tempValues.clear()
-#                 return tempValues
-#     return tempValues
-
-
-proc eliminate(values: var Table[string, string], s: string, d: string): bool =
+proc eliminate(values: var GridValues, s: string, d: string): bool =
     #[ Eliminate d from values[s]; propagate when values or places <= 2.
     Return values, except return empty table if a contradiction is detected. ]#
     
@@ -440,7 +382,7 @@ proc eliminate(values: var Table[string, string], s: string, d: string): bool =
                 return false
     return true
 
-proc display(values: Table[string, string]) =
+proc display*(values: GridValues) =
     # Display these values as a 2D grid.
     # let maxValuesLen = max(values.values.toSeq.map(x => x.len))
     
@@ -473,20 +415,20 @@ proc display(values: Table[string, string]) =
             echo line
 
 #let grid1 = "4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......"
-let grid1 = "003020600900305001001806400008102900700000008006708200002609500800203009005010300"
-let parsed_grid = parse_grid(grid1)
-echo "grid1: ", grid1
+# let grid1 = "003020600900305001001806400008102900700000008006708200002609500800203009005010300"
+# let parsed_grid = parse_grid(grid1)
+# echo "grid1: ", grid1
 # echo "parsed_grid: ", parsed_grid
 # echo "parsed_grid type: ", type(parsed_grid)
 # assert len(parsed_grid) == 81
 # echo "Parsed grid / starting grid:"
-display(parsed_grid)
+# display(parsed_grid)
 # Should be completely solved by parser since this is one of the easier Project Euler sudoku puzzles
 
 
-proc search(values: var Table[string, string]): bool
+proc search(values: var GridValues): bool
 
-proc solve(grid: string): Table[string, string] =
+proc solve*(grid: string): GridValues =
     var values = parse_grid(grid)
     # echo "Starting grid:"
     # display(values)
@@ -494,7 +436,7 @@ proc solve(grid: string): Table[string, string] =
     assert solved == true
     return values
 
-proc solve(values: Table[string, string]): Table[string, string] =
+proc solve*(values: GridValues): GridValues =
     var valuesCopy = values
     let solved = search(valuesCopy)
     assert solved == true
@@ -527,19 +469,135 @@ proc search(values: var Table[string, string]): bool =
                 values = valuesCopy  # Copy solution to values
                 # echo "assigned/valuesCopy:"
                 # display(valuesCopy)
-                echo "Solved!!!"
+                # echo "Solved!!!"
                 return solveSuccess
     return false
 
-# let solved = solve(grid1)
-# echo "Solved:"
-# display(solved)
+proc gridValuesAsString(grid: GridValues): string =
+    var lines: seq[string] = @[]
+    let rowSeparator = "-------+-------+-------"
+    for y, row in rows:
+        
+        # let row = grid[y]       
+        var rowStr: string
+        # echo "row: ", row
+        for x, col in cols:
+            let cellValues = grid[row & col]
+            if len(cellValues) > 1 or (len(cellValues) == 1 and cellValues[0] in "0."):
+                # Display as blank as there are multiple possible values for the cell
+                rowStr.add(" -")  
+            else:
+                rowStr.add(" " & cellValues)
+            
+            if col in "36":
+                rowStr.add(" |")
+        lines.add(rowStr & " ".repeat(len(rowSeparator) - len(rowStr)))
+        if row in "CF":
+            lines.add(rowSeparator)
+    result = lines.join("\n")
 
-let grid2 = "4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......"
-let parsed = parse_grid(grid2)
-echo "Parsed:"
-echo parsed
-display(parsed)
-let solved = solve(parsed)
-echo "Solved:"
-display(solved)
+proc displayTwoGridValues*(grid1: GridValues, grid2: GridValues, grid1_name: sink string, grid2_name: string) =
+    var grid1_lines = gridValuesAsString(grid1).splitLines()
+    var grid2_lines = gridValuesAsString(grid2).splitLines()
+
+    grid1_name = grid1_name & ":"
+    echo fmt"{grid1_name:<26} {grid2_name}:"
+    for i, (grid1_line, grid2_line) in zip(grid1_lines, grid2_lines):
+        # echo grid1_line & "      " & grid2_line
+        echo fmt"{grid1_line:<26} {grid2_line}"
+        # echo "grid1_line: ", grid1_line
+        # echo "grid2_line: ", grid2_line
+
+
+proc solveFile(filePath: string) =
+    # Solve and display results of all sudokus puzzles in a file
+    echo "Solving..."
+
+    const allowedChars = "0123456789."
+    const allowedCharSet = allowedChars.toSet()
+
+    var contents: string
+    var grids: seq[string]
+    for line in readFile(filePath).strip().splitLines():
+        var line = line.strip()
+        if line.len < 9:
+            continue
+
+        if not (line.toSet <= allowedCharSet):
+            # line's character isn't a subset of allowedCharSet.
+            continue
+        
+        contents &= line
+        while contents.len >= len(squares):
+            grids.add(contents[0 ..< len(squares)])
+            contents = contents[len(squares) ..< len(contents)]
+
+    var parsedGrids: seq[GridValues]  # Contains grids with potential values for each cell (partially solved)
+    var unparsedGrids: seq[GridValues]  # Contains grids in their initial, unsolved state
+    
+    for grid in grids:
+        let parsedGrid = parse_grid(grid)
+        parsedGrids.add(parsedGrid)
+        
+        let unparsedGrid: GridValues = grid_values(grid)
+        unparsedGrids.add(unparsedGrid)
+    
+    for i, grid in parsedGrids:
+        var solvedGrid = solve(grid)
+        displayTwoGridValues(unparsedGrids[i], solvedGrid, fmt"Puzzle #{i + 1}", "Solved")
+        echo ""
+        
+    
+    
+
+proc main() =
+
+    var stringArg1: string
+
+    for (kind, key, val) in getopt(commandLineParams()):
+        case kind:
+        of cmdArgument:
+            stringArg1 = key
+            echo "Argument: ", key
+        of cmdLongOption, cmdShortOption:
+            if val == "":
+                echo "Option: ", key
+            else:
+                echo "Option and value: ", key, ", ", val
+        of cmdEnd:
+            doAssert(false)
+
+    if isEmptyOrWhitespace(stringArg1):
+        echo "No parameters detected. Have a demo instead."
+        let grid2 = "4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......"
+        let parsed = parse_grid(grid2)
+        # echo "Parsed:"
+        # echo parsed
+        # display(parsed)
+        let solved = solve(parsed)
+        # echo "Solved:"
+        # display(solved)
+
+        # echo "Parsed:"
+        # echo gridValuesAsString(parsed)
+
+        echo ""
+        displayTwoGridValues(parsed, solved, "Parsed", "Solved")
+        echo ""
+    
+    # echo "stringArg1: " & stringArg1
+    if fileExists(stringArg1):
+        echo "Passed parameter is a file."
+        solveFile(stringArg1)
+    else:
+        # Attempt to solve
+        try:
+            let parsed = parse_grid(stringArg1)
+            let solved = solve(parsed)
+            displayTwoGridValues(parsed, solved, "Parsed", "Sovled")
+        except AssertionDefect as exc:
+            echo "Unable to convert string argument to sudoku puzzle."
+            echo "Goodbye!"
+    
+
+main()
